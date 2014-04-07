90e6db975d969d5ceddb6f33bac87e345f4296e5
Jeremy Green <jeremy@octolabs.com>
2014-04-06 21:31:17 -0500

    Initial commit. Generated new app with:
    rails new so_auth_client -T --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoAuthClient::Application.config.secret_key_base = 'c99779ae90ba902d37269521ec4d7a0baa7cf178c3b16742a98180807c71e0572ca27b01b5a81cfe349d60ca2a98e983ae7f7b1288bdb7534050dd0adee7d448'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoAuthClient::Application.config.session_store :cookie_store, key: '_so_auth_client_session'
